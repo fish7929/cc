ac2cceca5fb7a0f94214d7743e365eb7eb9da572
kennyNing <617877410@qq.com>
2017-07-07 15:07:25 +0800

    登录加好友功能demo

--- app/login.js
+++ app/login.js
@@ -66,11 +66,45 @@ user_id string 用户id
 friend pointer 好友对象
 
 当用户扫码进来关注用户
+
+每次关注操作都是互相的写入表数据，所以是2次数据的保存
  * 
  * 
  * **/
-lc_api.followeeUser = function () {
-
+lc_api.setFriend = function (user_id, current, cb_ok, cb_err) {
+  if (user_id == current.id) {
+    cb_ok("自己扫码自己登录");
+    return;
+  }
+    var f = AV.Object.extend('friend');
+    // 新建对象
+    var obj = new f();
+    obj.set('user_id', user_id);
+    obj.set('friend', current);
+    obj.save().then(function (todo) {
+      //根据user_id查询该用户对象
+      var query = new AV.Query('_User');
+      query.equalTo('objectId', user_id);
+      query.first().then(function (data) {
+        if (data) {
+          // 新建对象
+          var obj2 = new f();
+          obj2.set('user_id', current.id);
+          obj2.set('friend', data);
+          obj2.save().then(function (todo) {
+            cb_ok();
+          }, function (error) {
+            cb_err(error);
+          });
+        } else {
+          cb_ok(data);
+        }
+      }, function (error) {
+        cb_err(error);
+      });
+    }, function (error) {
+      cb_err(error);
+    });
 }
 
 //获取url的参数
@@ -89,11 +123,16 @@ lc_api.GetRequest = function () {
 
 
 //根据code获取用户对象
-lc_api.userOauthLogin = function (code) {
-  lc_api.getWXLogin(code, function (obj) {
+lc_api.userOauthLogin = function (code, user_id) {
+  lc_api.getWXLogin(code, function (data) {
     document.getElementById("contentDiv").innerHTML = "微信用户信息：" + JSON.stringify(data);
-    lc_api.getUserStatus(data, function (user) {
-      alert("注册完成并登录成功：" + AV.User.current().get("user_nick"));
+    lc_api.getUserStatus(data, function (user) {//进行登录或注册
+      if (user_id) {//存在user_id为扫码注册登录完成 
+        //去关注
+        lc_api.userQrcodeLogin(user_id);
+      } else {
+        alert("注册完成并登录成功：" + AV.User.current().get("user_nick"));
+      }
     }, function (error) {
       alert(error.message);
     });
@@ -102,46 +141,56 @@ lc_api.userOauthLogin = function (code) {
   });
 }
 
-//根据二维码注册登录进入系统，同时关注二维码用户
-lc_api.userQrcodeLogin = function (user) {
-  if(!user) return;
+//根据二维码注册登录进入系统，同时加二维码用户为好友
+lc_api.userQrcodeLogin = function (user_id) {
+  if (!user_id) return;
 
-  //判断当前登录状态
-  var current = AV.User.current();
-  var userFriendLiset=[];
+  var userFriendList = [];
 
-  var checkFriend=function(uid){
-      
-      for(var i=0;i<userFriendLiset;i++){
-         if(uid==userFriendLiset[i].id){
-             return true;
-         }
+  var checkFriend = function (uid) {
+    var b=false;
+    for (var i = 0; i < userFriendList.length; i++) {
+      if (uid == userFriendList[i].get("friend").id) {
+        b=true;
+        break;
       }
+    }
+    return b;
   }
-
-  var query = new AV.Query('friend');
-  query.equalTo('user_id', user);
-  query.limit(1000);
-  query.find().then(function (results) {//查找出二维码的所有好友
-    userFriendLiset=results;
-
-  }, function (error) {
-     alert(error);
-  });
-
-  //判断当前登录状态
   var current = AV.User.current();
   if (current) {//1.已登录未关注，关注二维码用户，2.已登录已关注，进入系统页面
-
-  } else {//1.未登录已关注，先登录再去系统页面，2.未登录未关注，先注册登录
-
+    var query = new AV.Query('friend');
+    query.equalTo('user_id', user_id);
+    query.limit(1000);
+    query.find().then(function (results) {//查找出二维码用户的所有好友 
+      if (results.length > 0) {
+        userFriendList = results; 
+        if (checkFriend(current.id) == true) {
+          //去到系统页面
+          alert("去系统页面");
+        } else {
+          //进行关注
+          lc_api.setFriend(user_id, current, function (obj) {
+            alert("去系统页面");
+          }, function (error) {
+            alert(error);
+          });
+        }
+      } else {//如果二维码用户没有好友
+        //进行关注
+        lc_api.setFriend(user_id, current, function (obj) {
+          alert("去系统页面");
+        }, function (error) {
+          alert(error);
+        });
+      }
+    }, function (error) {
+      alert(error);
+    });
+  } else {
+    //注意,成功回调地址多了个user_id参数
+    window.open("https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxf82fb57ce47d0df6&redirect_uri=http%3a%2f%2fwww.6itec.com%2fshare%2fdemo%2flogincallback.html?user_id=" + user_id + "&response_type=code&scope=snsapi_login&state=" + new Date().getTime() + "#wechat_redirect", "_self");
   }
-
-  lc_api.getUserStatus(data, function (user) {
-    alert("注册完成并登录成功：" + AV.User.current().get("user_nick"));
-  }, function (error) {
-    alert(error.message);
-  });
 
 }
 
