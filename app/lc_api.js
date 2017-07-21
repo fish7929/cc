1f963f2a6b17e0462623c6539682af7d1546b17d
fishYu <kingfish86@163.com>
2017-07-21 22:58:18 +0800

    更新 最新

--- app/lc_api.js
+++ app/lc_api.js
@@ -606,7 +606,7 @@ lc_api.initWXShare = function (user) {
     var obj = JSON.parse(objs);
     try {
       wx.config({
-        debug: true,//开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+        debug: false,//开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
         appId: obj.data.appid,
         timestamp: obj.data.timestamp,
         nonceStr: obj.data.noncestr,
@@ -624,10 +624,10 @@ lc_api.initWXShare = function (user) {
           wx.onMenuShareTimeline({
             title: _title, // 分享标题
             link: link, // 分享链接
-            imgUrl: 'http://www.6itec.com/share/share-logo.png', // 分享图标
+            imgUrl: 'http://www.6itec.com/test/share-logo.png', // 分享图标
             success: function () {
               console.log("node api朋友圈分享成功");
-              alert("node api朋友圈分享成功");
+              // alert("node api朋友圈分享成功");
             },
             cancel: function () {
               console.log('onMenuShareTimeline失败')
@@ -639,7 +639,7 @@ lc_api.initWXShare = function (user) {
             title: _title, // 分享标题
             desc: "快来生成属于你的英雄执照吧", // 分享描述
             link: link, // 分享链接
-            imgUrl: 'http://www.6itec.com/share/share-logo.png', // 分享图标
+            imgUrl: 'http://www.6itec.com/test/share-logo.png', // 分享图标
             type: 'link', // 分享类型,music、video或link，不填默认为link
             dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
             success: function () {
